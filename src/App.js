e573935cb4af887965dcaa94a4fe47882d959627
Cchanyu <Chanyu96@gmail.com>
2020-05-22 13:47:13 -0400

    more components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
 import "./App.scss";
 
 class App extends Component{
@@ -9,13 +10,16 @@ class App extends Component{
       <div className="app">
         <Router>
           <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <div className="app__container">
-              
-              </div>
-            </Route>
-          </Switch>
+          <div className="app__content">
+            <Sidebar />
+            <Switch>
+              <Route exact path="/">
+                <div className="app__container">
+                
+                </div>
+              </Route>
+            </Switch>
+          </div>
         </Router>
       </div>
     );
