496077c5a32a2878a082b6166110f87025f3f36b
Cchanyu <Chanyu96@gmail.com>
2020-05-23 16:33:31 -0400

    Updates

---
+++ precache-manifest.37401f59efd71d21d8d1eccb33d9dcba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b97644391097fdf106639ca36490a9b",
+    "url": "/CCPortfolio/index.html"
+  },
+  {
+    "revision": "39a38a37048739edc61c",
+    "url": "/CCPortfolio/static/css/main.543468ac.chunk.css"
+  },
+  {
+    "revision": "b87ea86b0a545f0f8d5b",
+    "url": "/CCPortfolio/static/js/2.aeb9bb63.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CCPortfolio/static/js/2.aeb9bb63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a38a37048739edc61c",
+    "url": "/CCPortfolio/static/js/main.bef3d7d3.chunk.js"
+  },
+  {
+    "revision": "6edf475580b5c1f25989",
+    "url": "/CCPortfolio/static/js/runtime-main.bb66afeb.js"
+  },
+  {
+    "revision": "368a597cf5a886c6b0e39406acfc5eab",
+    "url": "/CCPortfolio/static/media/user-solid.368a597c.svg"
+  }
+]);
