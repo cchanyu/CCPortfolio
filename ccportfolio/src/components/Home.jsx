72d5b46437bdf4adf62f1ffa1c5e711bd81e7782
cchanyu <chanyu96@gmail.com>
2023-01-31 00:29:32 -0500

    CC - react spline added

--- ccportfolio/src/components/Home.jsx
+++ ccportfolio/src/components/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../css/Home.css';
-// import Spline from '@splinetool/react-spline';
+import Spline from '@splinetool/react-spline';
 
 const Home = () => {
     return(
         <div className="home">
-            {/* <Spline scene="https://prod.spline.design/iSTpEEfVMef0QNyF/scene.splinecode" /> */}
+            <Spline scene="https://prod.spline.design/iSTpEEfVMef0QNyF/scene.splinecode" />
         </div>
     )
 }
